08ec0567d5225035f6fc99b5e1c70dd89c637c68
JeroenVinke <jeroenvinke17@gmail.com>
2017-06-28 16:48:15 +0200

    remove unused command in package scripts

--- skeleton-typescript-webpack-experimental/package-scripts.js
+++ skeleton-typescript-webpack-experimental/package-scripts.js
@@ -86,11 +86,7 @@ module.exports = {
       server: {
         default: `webpack-dev-server -d --devtool '#source-map' --inline --env.server`,
         extractCss: `webpack-dev-server -d --devtool '#source-map' --inline --env.server --env.extractCss`,
-        hmr: `webpack-dev-server -d --devtool '#source-map' --inline --hot --env.server`,
-        ssr: concurrent({ 
-          build: 'webpack -d --env.extractCss --watch',
-          run: 'nodemon -L --watch server/index.ts --watch server/aurelia-ssr-router.ts --watch server/aurelia-koa-middleware.ts --watch server/aurelia-ssr-renderer.ts --watch server/serve.ts --watch server/interfaces.ts --exec "ts-node server/index"'
-        })
+        hmr: `webpack-dev-server -d --devtool '#source-map' --inline --hot --env.server`
       },
     },
     serve: 'http-server dist --cors'
