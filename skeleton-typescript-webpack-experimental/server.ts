60fdaa24b290aace239d5171548d855a6937680c
Jeroen Vinke <JeroenVinke17@gmail.com>
2017-07-03 16:49:00 +0200

    koa page caching

--- skeleton-typescript-webpack-experimental/server.ts
+++ skeleton-typescript-webpack-experimental/server.ts
@@ -1,4 +1,6 @@
 import * as Koa from 'koa';
+import * as koaCash from 'koa-cash';
+import * as NodeCache from 'node-cache';
 import * as path from 'path';
 import {setup} from './ssr-engine/aurelia-ssr-engine';
 import {aureliaKoaMiddleware} from './koa-middleware/aurelia-koa-middleware';
@@ -9,6 +11,26 @@ setup();
 var port = process.env.PORT || 8080;
 
 const app = new Koa();
+const nodeCache = new NodeCache();
+const cachedPages = /^\/users/;
+
+app.use(koaCash({
+  get: (key) => {
+    return nodeCache.get(key);
+  },
+  set: (key, value) => {
+    nodeCache.set(key, value, 10000);
+  }
+}));
+
+app.use(async (ctx, next) => {
+  if (ctx.request.URL.pathname.match(cachedPages)) {
+    if (await ctx.cashed()) 
+      return;
+  }
+
+  await next();
+});
 
 app.use(aureliaKoaMiddleware({
   preboot: true,
